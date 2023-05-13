ffdeca95e1e9701aff91d92a99855f7b1212131d
Dmitrii126776 <dukek67@gmail.com>
2023-05-13 10:03:11 -0400

    react-typed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import retro from '../images/retrosupply.jpg';
-import Typed from 'react-typed';
+
 const resumePDF = require('../document/Dmitrii_Kuzhilin_developer_2023.docx');
 const Header = () => {
     const downloadResume = () => {
@@ -20,13 +20,13 @@ const Header = () => {
                             type="button" style={{marginTop: 20}}
                             onClick={downloadResume}
                     >Get my CV</button>
-                    <Typed
-                        className="typed-text"
-                        strings={["React JavaScript", "MongoDB ExpressJS", "Bootstrap Node.js", "Material-UI Redux", "TypeScript Next.js"]}
-                        typeSpeed={40}
-                        backSpeed={60}
-                        loop
-                    />
+                    {/*<Typed*/}
+                    {/*    className="typed-text"*/}
+                    {/*    strings={["React JavaScript", "MongoDB ExpressJS", "Bootstrap Node.js", "Material-UI Redux", "TypeScript Next.js"]}*/}
+                    {/*    typeSpeed={40}*/}
+                    {/*    backSpeed={60}*/}
+                    {/*    loop*/}
+                    {/*/>*/}
                     <a href="/projects" className="btn btn-secondary btn-lg "
                        tabIndex="-1" role="button" style={{marginTop: 20}}
                     >My projects</a>
