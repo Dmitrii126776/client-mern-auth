fbadd7996234dd0017c4fcbb3f260771abd1c3de
Dmitrii126776 <dukek67@gmail.com>
2023-05-12 23:03:16 -0400

    ContactForm.js

---
+++ src/components/ContactForm.js
@@ -0,0 +1,52 @@
+import React, {useState} from 'react';
+import {Modal, ModalHeader, ModalBody} from 'reactstrap';
+import {useForm, ValidationError} from "@formspree/react";
+
+function ContactForm({modal, toggle}) {
+    const [state, handleSubmit] = useForm("mlekvbpw");
+    const [address, setAddress] = useState('')
+    const [message, setMessage] = useState('')
+    // if (state.succeeded) {
+    //     return <p>Thanks for message!</p>;
+    // }
+    const handleCancel = () => {
+        setAddress('')
+        setMessage('')
+        toggle()
+    }
+    return (
+        <div>
+            <Modal isOpen={modal} toggle={handleCancel}>
+                <ModalHeader toggle={handleCancel}>Send Your Message</ModalHeader>
+                <ModalBody>
+                    <form onSubmit={handleSubmit}>
+                        <div className="input-group mb-3">
+                            <span className="input-group-text" id="basic-addon1">Your Email:</span>
+                            <input id="email" type="email" name="email"
+                                   value={address} onChange={e => setAddress(e.target.value)}
+                                   className="form-control"
+                                   placeholder="type your email address ..."/>
+                        </div>
+                        <ValidationError prefix="Email" field="email" errors={state.errors}/>
+                        <div className="input-group">
+                            <span className="input-group-text">Message:</span>
+                            <textarea id="message" name="message" placeholder="type your message ..."
+                                      className="form-control" aria-label="With textarea"
+                                      value={message} onChange={e => setMessage(e.target.value)}></textarea>
+                        </div>
+                        <ValidationError prefix="Message" field="message" errors={state.errors}/>
+                        <div style={{display: "flex", justifyContent: "center", marginTop: "15px"}}>
+                            <button className="btn btn-primary" type="submit"
+                                    style={{width: "100%"}}
+                                    disabled={state.submitting || address === '' || message === ''}>
+                                Submit Your Message
+                            </button>
+                        </div>
+                    </form>
+                </ModalBody>
+            </Modal>
+        </div>
+    );
+}
+
+export default ContactForm;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useLocation} from 'react-router-dom';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faGithub} from '@fortawesome/free-brands-svg-icons';
 import {faLinkedin} from '@fortawesome/free-brands-svg-icons';
+import {Button} from "reactstrap";
+import ContactForm from "./ContactForm";
 
 const Footer = () => {
     const location = useLocation();
@@ -10,24 +12,32 @@ const Footer = () => {
     // Set marginTop to 0 for the Welcome page and 20 for other pages
     const marginTop = location.pathname === '/' ? 0 : 15;
 
+    const [modal, setModal] = useState(false);
+    const toggle = () => setModal(!modal);
+
+    // <Button color="danger" onClick={toggle}>
+    //     Click Me
+    // </Button>
+
     return (
         <nav className="navbar navbar-expand-lg bg-secondary" style={{marginTop, maxHeight: 160}}>
             <div className="container-fluid d-flex justify-content-center">
                 <ul className="navbar-nav d-flex align-items-center">
                     <li className="nav-item"
                         style={{display: 'flex', alignItems: 'center'}}>
-                        <a href="mailto:kuzhilind@gmail.com"
-                           target="_blank" rel="noopener noreferrer" style={{padding: 0, marginRight: 0}}>
+                        <span onClick={toggle}
+                           style={{padding: 0, marginRight: 5, cursor: "pointer"}}>
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
                                  className="bi bi-envelope footer-item" viewBox="0 0 20 20">
                                 <path
                                     d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
                             </svg>
-                        </a>
-                        <a className="navbar-brand text-center footer-item"
-                           href="mailto:kuzhilind@gmail.com"
-                           target="_blank" rel="noopener noreferrer">Contact Me</a>
+                        </span>
+                        <span style={{cursor: "pointer"}} className="navbar-brand text-center footer-item"
+                           onClick={toggle}
+                        >Contact Me</span>
                     </li>
+                    <ContactForm toggle={toggle} modal={modal}/>
                     <li className="nav-item"
                         style={{display: 'flex', alignItems: 'center', marginLeft: 50, marginRight: 50}}>
                         <span className="github">
